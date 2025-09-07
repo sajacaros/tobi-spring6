30795a9d17dc933e3818ca3843803416c97a42c6
sajacaros <sajacall82@naver.com>
2025-09-07 19:59:10 +0900

    :sparkles: spring jpa transaction 반영

--- src/main/java/kr/study/spring6/DataClient.java
+++ src/main/java/kr/study/spring6/DataClient.java
@@ -4,8 +4,13 @@ import kr.study.spring6.configure.DataConfig;
 import kr.study.spring6.data.OrderRepository;
 import kr.study.spring6.order.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 
@@ -14,13 +19,20 @@ public class DataClient {
     public static void main(String[] args) throws InterruptedException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        new TransactionTemplate(transactionManager).execute(new TransactionCallback<Order>() {
+            @Override
+            public @Nullable Order doInTransaction(TransactionStatus status) {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-        log.info("save order, {}", order);
+                log.info("save order, {}", order);
 
-        Order order2 = new Order("100", BigDecimal.TEN);
-        repository.save(order2); // ConstraintViolationException 발생
+                Order order2 = new Order("100", BigDecimal.TEN);
+                repository.save(order2); // ConstraintViolationException 발생
+                return null;
+            }
+        });
     }
 }
